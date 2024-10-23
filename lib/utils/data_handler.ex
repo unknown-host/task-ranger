f00d2cb4d1c4a4cb6f09bd794c6647f71a76610f
Denis Krejcarek <denis.krejcarek@gmail.com>
2024-10-23 20:13:59 +0300

    add utils commit

---
+++ lib/utils/data_handler.ex
@@ -0,0 +1,20 @@
+defmodule DataHandler do
+  @moduledoc """
+  TODO: Load data from json file and convert it to list of maps.of valid numbers.
+
+  defp pos_num(i), do: is_integer(i) and i > -1
+  defp validate(data=[%{id: _, time: _, value: _} | _]) do
+    #Enum.all?(data, &is_map/0) and
+    Enum.all?(data, fn map ->
+      Enum.all?(map, fn {_key, value} -> pos_num(value) end)
+    end)
+  end
+  """
+
+  # @spec sanitizing_output(list(map()), list(atom())) :: map()
+  def sanitize_output(result), do: sanitize_output(result, [:coefficient])
+  def sanitize_output(result, keys) do
+    Enum.map(result, &Map.drop(&1, keys))
+  end
+
+end
